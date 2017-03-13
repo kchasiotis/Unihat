ae804093b36f6c552edb4b51877e9bb46196567e
Armageddonas <icsd11175@icsd.aegean.gr>
2017-03-13 19:54:48 +0200

    Code cleanup

--- index.android.js
+++ index.android.js
@@ -1,18 +1,10 @@
 // React imports
 import React, {Component} from 'react';
 import {
-    AppRegistry,
-    StyleSheet,
-    View
+    AppRegistry
 } from 'react-native';
-// var ReactNative = require('react-native');
-// var {
-//     AsyncStorage,
-// } = ReactNative;
 
 // UI componets imports
-import {Container, Header, Title, Content, Body} from 'native-base';
-import Spinner from 'react-native-loading-spinner-overlay';
 import MainScreen from './src/mainScreen/mainScreen'
 import Login from './src/login/Login'
 import IcarusCrawler from './src/icarusCrawler/IcarusCrawler'
@@ -24,15 +16,6 @@ export default class IcarusAegean extends Component {
 
         this.onLogin = this.onLogin.bind(this);
         this.logout = this.logout.bind(this);
-        // AsyncStorage.getItem('analyticGrades', (err, result) => {
-        //     this.setState({loading: false});
-        //     if (err) {
-        //         console.log(err);
-        //         return;
-        //     }
-        //
-        //     this.setState({analyticGrading: JSON.parse(result)});
-        // });
     }
 
     onLogin(state, aGrades) {
@@ -40,7 +23,6 @@ export default class IcarusAegean extends Component {
         console.log(state ? 'Logged in' : 'Log in failed');
         if (aGrades) {
             this.setState({analyticGrading: aGrades});
-            // AsyncStorage.setItem('analyticGrades', JSON.stringify(analGrades));
         }
     }
 
@@ -50,13 +32,6 @@ export default class IcarusAegean extends Component {
     }
 
     render() {
-        /*if (this.state.loading)
-         return (
-         <View style={{ flex: 1 }}>
-         <Spinner visible={true} textContent={"Loading..."} textStyle={{color: '#FFF'}}/>
-         </View>
-         );
-         */
         if (this.state.logged) {
             return <MainScreen logout={this.logout} aGrades={this.state.analyticGrading}/>
         }
@@ -66,24 +41,5 @@ export default class IcarusAegean extends Component {
         );
     }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
-});
 
 AppRegistry.registerComponent('IcarusAegean', () => IcarusAegean);
