81c1bee2bae193fd1bc4a85d1d033c377dd29198
Armageddonas <icsd11175@icsd.aegean.gr>
2017-03-20 19:19:01 +0200

    Performance improvement on LessonList

--- src/lessonList/lessonList.js
+++ src/lessonList/lessonList.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-import {ListItem, Right, Text, Badge, Body, List} from 'native-base';
-import {ScrollView} from 'react-native';
+import {ListItem, Right, Text, Badge, Body} from 'native-base';
+import {ScrollView, ListView} from 'react-native';
 
 function screenGrades(routeName, allGrades) {
     let grades;
@@ -34,31 +34,33 @@ function InitializeLessonList(gradesStr) {
 class LessonList extends Component {
     constructor(props) {
         super(props);
-
-        const listItems = this.props.grades.map((lesson) =>
-            <ListItem key={lesson.id}>
-                <Body>
-                <Text>{lesson.title}</Text>
-                </Body>
-                <Right>
-                    {
-                        lesson.grade !== '' ?
-                            <Badge success={lesson.grade>=5} danger={lesson.grade<5}>
-                                <Text>{lesson.grade}</Text>
-                            </Badge> :
-                            null
-                    }
-                </Right>
-            </ListItem>
-        );
-        this.state = {listItems: listItems}
+        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1.id !== r2.id});
+        this.state = {
+            dataSource: ds.cloneWithRows(this.props.grades),
+        };
     }
 
     render() {
         return (
-            <ScrollView>
-                {this.state.listItems}
-            </ScrollView>
+            <ListView
+                dataSource={this.state.dataSource}
+                renderRow={(lesson) =>
+                    <ListItem key={lesson.id}>
+                        <Body>
+                        <Text>{lesson.title}</Text>
+                        </Body>
+                        <Right>
+                            {
+                                lesson.grade !== '' ?
+                                    <Badge success={lesson.grade>=5} danger={lesson.grade<5}>
+                                        <Text>{lesson.grade}</Text>
+                                    </Badge> :
+                                    null
+                            }
+                        </Right>
+                    </ListItem>
+}
+            />
         );
     }
 }
