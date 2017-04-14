63b81c3d5de41c879fb91ab9546ddcfa6e9e0067
Armageddonas <icsd11175@icsd.aegean.gr>
2017-04-14 15:03:11 +0300

    Adds icons package

--- android/app/src/main/java/com/icarusaegean/MainApplication.java
+++ android/app/src/main/java/com/icarusaegean/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.oblador.keychain.KeychainPackage;
 import com.horcrux.svg.RNSvgPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new KeychainPackage(),
             new RNSvgPackage(),
             new RNFetchBlobPackage()
