89bd7a20767bba472cf883a1bec98a66bc4551b8
Armageddonas <icsd11175@icsd.aegean.gr>
2017-04-27 18:15:12 +0300

    Replaces listview with flatlist

--- src/lessonList/lessonList.js
+++ src/lessonList/lessonList.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {ListItem, Right, Text, Badge, Body} from 'native-base';
-import {ListView} from 'react-native';
+import {FlatList} from 'react-native';
 
 function InitializeLessonList(grades) {
     return function LessonListEm(props) {
@@ -11,37 +11,29 @@ function InitializeLessonList(grades) {
 }
 
 class LessonList extends Component {
-    constructor(props) {
-        super(props);
-        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1.id !== r2.id});
-        this.state = {
-            dataSource: ds.cloneWithRows(this.props.grades),
-        };
-    }
-
     render() {
-        if(this.props.grades.length === 0 ) return <Text>Άδεια λίστα μαθημάτων</Text>;
+        if (this.props.grades.length === 0) return <Text>Άδεια λίστα μαθημάτων</Text>;
 
         return (
-            <ListView
-                dataSource={this.state.dataSource}
-                renderRow={(lesson) =>
-                    <ListItem key={lesson.id}>
+            <FlatList
+                keyExtractor={(item) => (item.id)}
+                data={this.props.grades}
+                renderItem={({item}) =>
+                    <ListItem>
                         <Body>
-                        <Text>{lesson.title}</Text>
+                        <Text>{item.title}</Text>
                         </Body>
                         <Right>
                             {
-                                lesson.grade !== '' ?
-                                    <Badge success={lesson.grade>=5} danger={lesson.grade<5}>
-                                        <Text>{lesson.grade}</Text>
+                                item.grade !== '' ?
+                                    <Badge success={item.grade >= 5} danger={item.grade < 5}>
+                                        <Text>{item.grade}</Text>
                                     </Badge> :
                                     null
                             }
                         </Right>
                     </ListItem>
                 }
-                initialListSize={7}
             />
         );
     }
