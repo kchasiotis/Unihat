773b76894d5961c5e4b65850e6f8f0514623b475
Armageddonas <icsd11175@icsd.aegean.gr>
2017-06-12 18:44:36 +0300

    Adds wrongly removed dependencies

--- android/app/src/main/java/com/icarusaegean/MainApplication.java
+++ android/app/src/main/java/com/icarusaegean/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.oblador.keychain.KeychainPackage;
 import com.facebook.react.ReactInstanceManager;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SvgPackage(),
             new VectorIconsPackage(),
             new KeychainPackage()
       );
