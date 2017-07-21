2da6f9005f12ef79a1e5adf876e05774ed82f63c
Armageddonas <icsd11175@icsd.aegean.gr>
2017-07-21 16:59:07 +0300

    1000th roll back on dependencies Argggggg

--- scripts/watchmanIncreaseLimits.sh
+++ scripts/watchmanIncreaseLimits.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-echo 256 | sudo tee -a /proc/sys/fs/inotify/max_user_instances
-echo 32768 | sudo tee -a /proc/sys/fs/inotify/max_queued_events
-echo 65536 | sudo tee -a /proc/sys/fs/inotify/max_user_watches
-watchman shutdown-server
+echo 999999 | sudo tee -a /proc/sys/fs/inotify/max_user_watches     && \
+  echo 999999 | sudo tee -a /proc/sys/fs/inotify/max_queued_events  && \
+  echo 999999 | sudo tee -a /proc/sys/fs/inotify/max_user_instances && \
+  watchman shutdown-server
