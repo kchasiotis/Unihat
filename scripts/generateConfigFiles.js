7506d6dd860ca82b63b5191106a380d921940c81
Armageddonas <icsd11175@icsd.aegean.gr>
2017-09-20 17:06:26 +0300

    Generate config files on install **Breaking change** Run `npm install` after pulling this commit

---
+++ scripts/generateConfigFiles.js
@@ -0,0 +1,35 @@
+let fs = require('fs');
+
+if (!fs.existsSync("./src/tools/crawler/icarusCrawler/.user.js")) {
+    fs.writeFile("./src/tools/crawler/icarusCrawler/.user.js", "export default user = {username:'', password:''}", function (err) {
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("The file was saved!");
+    });
+}
+
+
+if (!fs.existsSync("./src/tools/crawler/sefCrawler/.user.js")) {
+    fs.writeFile("./src/tools/crawler/sefCrawler/.user.js", "export default user = {username:'', password:''}", function (err) {
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("The file was saved!");
+    });
+}
+
+
+if (!fs.existsSync("./src/tools/api/.config.js")) {
+    fs.writeFile("./src/tools/api/.config.js", "export default user = {username:'', password:''}", function (err) {
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("The file was saved!");
+    });
+}
+
+
--- src/tools/api/.config.js
+++ src/tools/api/.config.js
@@ -1,3 +1,1 @@
-export default config = {
-    serverUrl: 'http://192.168.1.12:3000/lesson'
-}
+export default user = {username:'', password:''}
