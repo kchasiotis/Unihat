8e8dd8525205bf76f361591e7dc4bdbfcac873c6
Armageddonas <icsd11175@icsd.aegean.gr>
2017-10-01 23:42:17 +0300

    Minor removal

--- src/modules/lessonList/components/filter/checkBoxBtm.js
+++ src/modules/lessonList/components/filter/checkBoxBtm.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {CheckBox, ListItem, Text, View} from "native-base";
 import Theme from "../../../../../theme/components/checkBoxBtm";
 
