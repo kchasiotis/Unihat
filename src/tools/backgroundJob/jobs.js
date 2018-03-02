458e18b657e84e37fc02b4d3530bb6abbd6678d1
Armageddonas <icsd11175@icsd.aegean.gr>
2018-03-02 15:57:27 +0200

    Code refactoring

---
+++ src/tools/backgroundJob/jobs.js
@@ -0,0 +1,61 @@
+import {Crawler} from '../crawler'
+import {CredentialStorage, LocalStorage} from '../../tools/localStorage';
+import {Logger, LogFormat} from '../../tools/logger';
+
+let PushNotification = require('react-native-push-notification');
+let crawler = new Crawler();
+
+const jobNames = {newGradeCheck: {wifi: 'newGradeCheckWifi', mobile: 'newGradeCheckMobile'}};
+
+const newGradeCheckJob = (jobName) => {
+    return {
+        jobKey: jobName,
+        job: () => {
+            Logger.info('Running ' + jobName, LogFormat.HEADER);
+
+            CredentialStorage.load((error, username, password) => {
+                    if (error) {
+                        Logger.log(error);
+                        return;
+                    }
+
+                    crawler.fetchPage(username, password, (error, logged, serverGrades) => {
+                        if (error) {
+                            Logger.error(error);
+                            return;
+                        }
+
+                        if (logged) {
+                            LocalStorage.loadLessonsLists((error, storedGrades) => {
+                                if (error) {
+                                    Logger.error(error);
+                                    return;
+                                }
+
+                                let storedExGrades = storedGrades.exGrades;
+                                let serverExGrades = serverGrades.exGrades.filter((lesson) => lesson.grade !== null);
+
+                                for (let i = 0; i < storedExGrades.length; i++) {
+                                    for (let j = 0; j < serverExGrades.length; j++) {
+                                        if (storedExGrades[i].code === serverExGrades[j].code && storedExGrades[i].grade !== serverExGrades[j].grade) {
+                                            PushNotification.localNotification({
+                                                title: "Νέα βαθμολογία", // (optional, for iOS this is only used in apple watch, the title will be the app name on other iOS devices)
+                                                message: serverExGrades[j].title + ' ' + serverExGrades[j].grade.toString(), // (required)
+                                                playSound: true, // (optional) default: true
+                                                soundName: 'default', // (optional) Sound to play when the notification is shown. Value of 'default' plays the default sound. It can be set to a custom sound such as 'android.resource://com.xyz/raw/my_sound'. It will look for the 'my_sound' audio file in 'res/raw' directory and play it. default: 'default' (default sound is played)
+                                            });
+                                            LocalStorage.setLessonsLists(serverGrades);
+                                            LocalStorage.setRefreshLessonsListsCond(true);
+                                        }
+                                    }
+                                }
+                            })
+                        }
+                    })
+                }
+            )
+        }
+    }
+};
+
+export {newGradeCheckJob, jobNames};
---
+++ src/tools/backgroundJob/scheduler.js
@@ -0,0 +1,30 @@
+import {jobNames} from "./index";
+import BackgroundJob from "react-native-background-job";
+import env from "../../../environment";
+
+export default class Scheduler {
+    static run() {
+        let newGradeCheckScheduleWifi = {
+            jobKey: jobNames.newGradeCheck.wifi,
+            timeout: 15000,
+            period: 60 * 60 * 1000,
+            override: true,
+            networkType: BackgroundJob.NETWORK_TYPE_UNMETERED
+        };
+
+        // todo: (priority 2) change networkType to mobile
+        let newGradeCheckScheduleMobile = {
+            jobKey: jobNames.newGradeCheck.mobile,
+            timeout: 15000,
+            period: env.debug && env.shortSchedule ? 5 * 1000 : 12 * 60 * 60 * 1000,
+            override: true,
+            networkType: BackgroundJob.NETWORK_TYPE_ANY
+        };
+        BackgroundJob.schedule(newGradeCheckScheduleWifi);
+        BackgroundJob.schedule(newGradeCheckScheduleMobile);
+    };
+
+    static cancelAll() {
+        BackgroundJob.cancelAll();
+    }
+}
