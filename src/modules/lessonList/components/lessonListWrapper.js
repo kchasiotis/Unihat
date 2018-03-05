054ce5f583fee1cdfc039762f4f3c38ce4295ee8
Armageddonas <icsd11175@icsd.aegean.gr>
2018-03-05 11:18:44 +0200

    Improves fab button

--- src/modules/lessonList/components/lessonListWrapper.js
+++ src/modules/lessonList/components/lessonListWrapper.js
@@ -3,6 +3,43 @@ import {LessonList} from '../containers/lessonList';
 import {StatusBar, View} from "react-native";
 import {Fab, Icon} from "native-base";
 
+class FabFilter extends React.Component {
+    state = {pressable: true};
+    pressable = true;
+
+    componentDidMount() {
+        this.pressable = true;
+
+        this.didBlurSubscription = this.props.navigation.addListener(
+            'willFocus',
+            () => {
+                this.pressable = true;
+            }
+        );
+    }
+
+    componentWillUnmount(){
+        // Remove the listener when you are done
+        this.didBlurSubscription.remove();
+    }
+
+    render() {
+        return (
+            <Fab
+                direction="up"
+                style={{backgroundColor: 'rgba(105, 114, 104, 1)'}}
+                position="bottomRight"
+                onPress={() => {
+                    if (this.pressable === false) return;
+                    this.pressable = false;
+                    this.props.navigation.navigate('filter');
+                }}>
+                <Icon name="ios-funnel"/>
+            </Fab>
+        );
+    }
+}
+
 class LessonListWrapper extends React.Component {
     render() {
         const {navigation} = this.props;
@@ -16,13 +53,7 @@ class LessonListWrapper extends React.Component {
                 <LessonList routeName={navigation.state.routeName} navigation={navigation}/>
                 {
                     navigation.state.routeName === 'aGrades' ?
-                        <Fab
-                            direction="up"
-                            style={{backgroundColor: 'rgba(105, 114, 104, 1)'}}
-                            position="bottomRight"
-                            onPress={() => navigation.navigate('filter')}>
-                            <Icon name="ios-funnel"/>
-                        </Fab>
+                        <FabFilter navigation={navigation}/>
                         : null
                 }
             </View>
